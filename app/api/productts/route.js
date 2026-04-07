1ff35be7808b287f0a495b184886d72a5399025a
Vinoth <vinothelango2110@gmail.com>
2026-04-07 15:55:45 +0530

    debug mongodb issue

--- app/api/productts/route.js
+++ app/api/productts/route.js
@@ -3,22 +3,32 @@ import { connectDB } from "@/lib/mongodb";
 
 export async function GET() {
   try {
+    console.log("🔥 API HIT");
+
+    // Check ENV
+    console.log("MONGO URI:", process.env.MONGODB_URI ? "FOUND" : "NOT FOUND");
+
     await connectDB();
+
+    console.log("✅ DB CONNECTED");
 
     const products = await mongoose.connection
       .collection("products")
       .find({})
       .toArray();
 
+    console.log("📦 PRODUCTS:", products.length);
+
     return Response.json({
       success: true,
       products,
     });
+
   } catch (error) {
-    console.log("ERROR:", error);
-    return Response.json(
-      { error: "Failed to fetch products" },
-      { status: 500 }
-    );
+    console.error("❌ FULL ERROR:", error);
+
+    return Response.json({
+      error: error.message,
+    }, { status: 500 });
   }
 }
