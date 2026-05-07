35581cfe0eb9ce029a27ee15e4340fcc99f41f9d
Vinoth <vinothelango2110@gmail.com>
2026-05-07 23:07:20 +0530

    Debug cloudinary env

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,9 +1,20 @@
 import { v2 as cloudinary } from "cloudinary";
 
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+const apiKey = process.env.CLOUDINARY_API_KEY;
+const apiSecret = process.env.CLOUDINARY_API_SECRET;
+
+console.log("CLOUDINARY ENV CHECK:");
+console.log({
+  cloudName,
+  apiKey,
+  apiSecretExists: !!apiSecret,
+});
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecret,
 });
 
 export default cloudinary;
